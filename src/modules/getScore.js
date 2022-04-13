d1ccc9bc83323bf23cbf210e3759a77d39e5c2a2
zkr024 <zkr.bm.02403@gmail.com>
2022-04-12 22:27:01 -0500

    API access
    replace localStorage for an API

--- src/modules/getScore.js
+++ src/modules/getScore.js
@@ -1,12 +1,11 @@
 import { form, participant, score } from './variables.js';
 import newScore from './scoreArray.js';
-import updateList from './addToList.js';
+import saveToAPI from './saveToAPI.js';
 
 const getScore = form.addEventListener('submit', (e) => {
   e.preventDefault();
   newScore.addScore(participant.value, score.value);
-  newScore.save();
-  updateList();
+  saveToAPI();
   form.reset();
 });
 
--- src/modules/refresh.js
+++
@@ -1,7 +0,0 @@
-import storeRecords from './storeRecords.js';
-
-const load = document.addEventListener('DOMContentLoaded', () => {
-  storeRecords();
-});
-
-export default load;
