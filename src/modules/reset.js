a513cfff34aa57a5907a20b75b3ebe1e08258ed3
zkr024 <zkr.bm.02403@gmail.com>
2022-04-12 22:31:57 -0500

    Linters issues

--- src/modules/reset.js
+++ src/modules/reset.js
@@ -1,5 +1,5 @@
 import { reset } from './variables.js';
-import getFromAPI from './getFromAPI.js'; 
+import getFromAPI from './getFromAPI.js';
 
 const refresh = reset.addEventListener('click', () => {
   window.location.reload();
